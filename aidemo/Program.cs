bd4843a375f9cdd11b4169c7093f625d655781ed
jalon <jbailey1502@gmail.com>
2024-07-01 14:25:21 -0400

    comit

---
+++ aidemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenAI_API;
+
+Console.WriteLine("Hello, World!");
+var api= new OpenAI_API(" ");
+var request = new OpenAI_AP.Images.ImageGenerationRequest{
+    Prompt = ""
+};
+var response = api.ImageGenerationRequest.CreateImageAsync(request);
+Console.WriteLine("Image URL: {0}", response.Date[0].URL);
